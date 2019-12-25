0fd0e72765eeccc4b4557570b6005d74f4ebc774
Bo Zhao <jakobzhao@gmail.com>
2019-12-24 20:11:32 -0800

    update

---
+++ 04_data/create_table.sql
@@ -0,0 +1,22 @@
+--created on Dec 24, 2020
+--@author:          Bo Zhao
+--@email:           zhaobo@uw.edu
+--@website:         https://hgis.uw.edu
+--@organization:    Department of Geography, University of Washington, Seattle
+--@description:     create a spatialite database on QGIS's database manager
+
+-- Create the data table
+CREATE TABLE IF NOT EXISTS geotweets (
+    id INTEGER PRIMARY KEY,
+    username  TEXT NOT NULL,
+    created_at TEXT DEFAULT 0,
+    lat REAL DEFAULT 0,
+    lng REAL DEFAULT 0,
+    text TEXT
+);
+
+-- Create the geometry column
+SELECT AddGeometryColumn('geotweets', 'geom', 4326, 'POINT', 'XY', 0);
+
+-- Update the geometry column
+update geotweets set the_geom = MakePoint(lng, lat, 4326)
--- 04_data/pe.md
+++ 04_data/pe.md
@@ -1,1 +1,6 @@
 # Spatial data management using SpatiaLite
+
+
+
+
+https://www.mongodb.com/download-center/community
