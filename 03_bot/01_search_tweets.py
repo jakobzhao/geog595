55598edb7ff4ea01c48e519d5b5e0a1e0454ce3a
Bo Zhao <jakobzhao@gmail.com>
2019-12-24 16:05:20 -0800

    update

--- 03_bot/01_search_tweets.py
+++ 03_bot/01_search_tweets.py
@@ -12,64 +12,50 @@ import time, datetime, json
 
 # url = "https://twitter.com/search?l=&q=near%3A%22houston%22%20within%3A15mi%20since%3A2017-08-24%20until%3A2017-08-31&src=typd&lang=en"  #crawlling all the tweets posted near Houston during the Hurricane Harvey attacked period.
 url = "https://twitter.com/search?l=&q=seattle&src=typd&lang=en"
+
 # use a chrome browser core. https://chromedriver.chromium.org/downloads
 browser = webdriver.Chrome(executable_path="assets/chromedriver.exe") # if you are a mac user, please use "assets/chromedriver"
 browser.get(url)
 
-
-time.sleep(8)
+f = open("assets/tweets.csv", "a", encoding="utf-8")
+f.write('user_id, user_name, screen_name, status_id, created_at, time_integer, reply_num, retweet_num, favorite_num, content \n')
+time.sleep(5)
 start = datetime.datetime.now()
+time_limit = 60
+texts = []
+
+# Read the Xpath tutorial if you are not familiar with XPath.
+# "//" operator indicates Selects nodes in the document from the current node that match the selection no matter where they are.
 while len(browser.find_elements_by_xpath('//div[contains(text(), "Back to top ↑")]')) != 1:
     time.sleep(5)
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     soup = BeautifulSoup(browser.page_source, 'html5lib')
-    items = soup.find_all('li', class_="stream-item")[-20:]
-
-    # if longer than a hour, then stop scrolling.
-    if int((datetime.datetime.now() - start).seconds / 60) > 60:
+    tweets = soup.find_all('li', class_="stream-item")[-20:] # only process the newly-acquired tweets.
+    if int((datetime.datetime.now() - start).seconds) >= time_limit: # if longer than a minute, then stop scrolling.
         break
-
-    for item in items:
+    for tweet in tweets:
         try:
-            user_json = json.loads(item.div.attrs["data-reply-to-users-json"])
+            user_json = json.loads(tweet.div.attrs["data-reply-to-users-json"])
             user_id = int(user_json[0]['id_str'])
             user_name = user_json[0]['screen_name']
             screen_name = user_json[0]['name']
-            status_id = int(item.attrs["data-item-id"])
-
-            content = item.find("p").text
-            created_at = item.find("small", class_="time").a.attrs["title"]
-            time_integer = item.find("small", class_="time").a.span["data-time-ms"]
-            reply_num = item.find("div", class_="ProfileTweet-action--reply").find("span", class_="ProfileTweet-actionCountForPresentation").text
-            retweet_num = item.find("div", class_="ProfileTweet-action--retweet").find("span", class_="ProfileTweet-actionCountForPresentation").text
-            favorite_num = item.find("div", class_="ProfileTweet-action--favorite").find("span", class_="ProfileTweet-actionCountForPresentation").text
-
+            status_id = int(tweet.attrs["data-item-id"])
+            text = tweet.find("p").text.strip().replace("\n", "")
+            created_at = tweet.find("small", class_="time").a.attrs["title"]
+            time_integer = tweet.find("small", class_="time").a.span["data-time-ms"]
+            reply_num = tweet.find("div", class_="ProfileTweet-action--reply").find("span", class_="ProfileTweet-actionCountForPresentation").text
+            retweet_num = tweet.find("div", class_="ProfileTweet-action--retweet").find("span", class_="ProfileTweet-actionCountForPresentation").text
+            favorite_num = tweet.find("div", class_="ProfileTweet-action--favorite").find("span", class_="ProfileTweet-actionCountForPresentation").text
             inst_url = ""
-            if "www.instagram.com" in content:
-                inst_url = item.p.a.attrs["title"]
+            if "www.instagram.com" in text:
+                inst_url = tweet.p.a.attrs["title"]
+            record = '%d, %s, %s, %d, %s， %s， %s， %s， %s， %s \n' % (user_id, user_name, screen_name, status_id, created_at, time_integer, reply_num, retweet_num, favorite_num, text)
+            print(record)
+            if (text not in texts):
+                f.write(record)
+            texts.append(text)
         except:
             pass
-
-        print(user_id, user_name, screen_name, status_id, content, created_at, time_integer, reply_num, retweet_num, favorite_num)
-
-        page_json = {
-            "user_id": user_id,
-            "user_name": user_name,
-            "screen_name": screen_name,
-            "status_id": status_id,
-            "content": content,
-            "inst_url": inst_url,
-            "created_at": created_at,
-            "time_integer": time_integer,
-            "reply_num": reply_num,
-            "retweet_num": retweet_num,
-            "favorite_num": favorite_num
-        }
-        # try:
-        #     db.tweets.insert_one(page_json)
-        # except errors.DuplicateKeyError:
-        #     print('This post has already been inserted.')
-
-
+f.close()
 browser.close()
 print("finished")
